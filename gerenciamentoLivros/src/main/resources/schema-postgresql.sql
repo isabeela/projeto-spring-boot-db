9c659286163a625ef9c5caa2aef89585a9ef6983
isabeela <31148780+isabeela@users.noreply.github.com>
2021-11-11 10:49:28 -0300

    add nova tabela
    add nova tabela

--- gerenciamentoLivros/src/main/resources/schema-postgresql.sql
+++ gerenciamentoLivros/src/main/resources/schema-postgresql.sql
@@ -5,3 +5,15 @@ CREATE TABLE IF NOT EXISTS lista (
      site   varchar (70),
      preco  varchar (40)
 );
+
+
+CREATE TYPE genero AS ENUM('Aventura', 'Auto-Ajuda', 'Biografia', 'Conto',
+'Didático/Educaional', 'Distopia', 'Drama', 'Fantasia', 'Ficção', 'Poema/Poesia', 
+'Policial', 'Romance', 'Suspense', 'Terror');
+
+CREATE TABLE IF NOT EXISTS proximas (
+     id     serial PRIMARY KEY,
+     livro  varchar(70),
+     autor  varchar(50),
+     genero_livro genero
+);
