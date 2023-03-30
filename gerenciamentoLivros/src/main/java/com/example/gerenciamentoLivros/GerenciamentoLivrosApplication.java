7639b600c09216564b6b53a23b85b78ae6b78065
isabeela <31148780+isabeela@users.noreply.github.com>
2023-03-30 14:34:19 -0300

    Update GerenciamentoLivrosApplication.java

--- gerenciamentoLivros/src/main/java/com/example/gerenciamentoLivros/GerenciamentoLivrosApplication.java
+++ gerenciamentoLivros/src/main/java/com/example/gerenciamentoLivros/GerenciamentoLivrosApplication.java
@@ -2,10 +2,10 @@ package com.example.gerenciamentoLivros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
-@ComponentScan
+
 public class GerenciamentoLivrosApplication {
 
 	public static void main(String[] args) {
