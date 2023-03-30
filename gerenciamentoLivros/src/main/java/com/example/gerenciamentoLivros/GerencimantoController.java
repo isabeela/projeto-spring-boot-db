1b6c3754b6d6163e3630b3662cbd2ea0f007798d
isabeela <31148780+isabeela@users.noreply.github.com>
2023-03-30 16:12:41 -0300

    Update GerencimantoController.java

--- gerenciamentoLivros/src/main/java/com/example/gerenciamentoLivros/GerencimantoController.java
+++ gerenciamentoLivros/src/main/java/com/example/gerenciamentoLivros/GerencimantoController.java
@@ -8,12 +8,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class GerencimantoController {
 	
-	@GetMapping("/")
+	/*@GetMapping("/")
 	public String inicio () {
 		return "index";
-	}
+	}*/
 	
-	@GetMapping("/inicio")
+	@GetMapping("/")
 	public String inicial () {
 		return "inicio";
 	}
