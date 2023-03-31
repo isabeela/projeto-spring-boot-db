daaf69f65fa2cbd47b5d6d8888209263be1336bb
isabeela <31148780+isabeela@users.noreply.github.com>
2023-03-31 12:23:24 -0300

    Update GerencimantoController.java

--- gerenciamentoLivros/src/main/java/com/example/gerenciamentoLivros/GerencimantoController.java
+++ gerenciamentoLivros/src/main/java/com/example/gerenciamentoLivros/GerencimantoController.java
@@ -1,11 +1,16 @@
 package com.example.gerenciamentoLivros;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@Controller
+@RestController
+//@Controller
+
+@RequestMapping("/api/")
 public class GerencimantoController {
 	
 	/*@GetMapping("/inicio")
